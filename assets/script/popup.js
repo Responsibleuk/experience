f9514225f2749930250ea9067044b88c6c414dec
Responsibleuk <james@we-are-responsible.com>
2022-01-23 19:27:21 +0000

    Add popup

---
+++ assets/script/popup.js
@@ -0,0 +1,9 @@
+
+   
+/* jshint esversion: 8 */
+
+window.onload = function() {
+    document.getElementById('button').onclick = function() {
+      document.getElementById('modalOverlay').style.display = 'none';
+    };
+  };
